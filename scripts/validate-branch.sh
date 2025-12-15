9c0b28945526ed7de2e2de3618b87ea5d53657a5
DongHyun <cdhppph@gamil.com>
2025-12-15 13:49:10 +0900

    chore: 폰트컬러세팅

--- scripts/validate-branch.sh
+++ scripts/validate-branch.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
-# TODO: branch naming rule will be added later
-exit 0
+branch=$(git symbolic-ref --short HEAD)
+
+echo "$branch" | grep -Eq '^(feat|fix|docs|style|refactor|test|chore)/#?[0-9]+-'
+
+if [ $? -ne 0 ]; then
+  echo "❌ 브랜치명이 규칙을 따르지 않습니다."
+  exit 1
+fi
